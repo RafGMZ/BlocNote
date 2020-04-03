15a76e0194391d24320deb2aea18bac93c3c205e
RafGMZ <62910061+RafGMZ@users.noreply.github.com>
2020-04-03 17:54:54 +0200

    Ajout de la sauvegarde du blocnote

--- app/src/main/java/com/rafgmz/blocnote/MainActivity.kt
+++ app/src/main/java/com/rafgmz/blocnote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rafgmz.blocnote
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val preferancesReader = getSharedPreferences("com.rafgmz.blocnote", Context.MODE_PRIVATE)
+        fullTextView.text = preferancesReader.getString("fullText", "")
     }
 
     fun addButtonTouched(Button:View) {
@@ -18,5 +21,8 @@ class MainActivity : AppCompatActivity() {
         val existingText = fullTextView.text
         fullTextView.text = "$existingText\n$userInput"
         userField.text = null
+        val preferancesManager = getSharedPreferences("com.rafgmz.blocnote", Context.MODE_PRIVATE).edit()
+        preferancesManager.putString("fullText", fullTextView.text as String)
+        preferancesManager.apply()
     }
 }
